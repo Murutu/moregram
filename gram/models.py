b7e5ce8703f6d74703d2e7dcc7d2337619f3a794
Murutu <murutupeter@email.com>
2020-03-08 18:42:12 +0300

    added 'django.template.context_processors.media' in settings.py

--- gram/models.py
+++ gram/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Profile(model.Model):
+class Profile(models.Model):
    profile_pic = models.ImageField(upload_to='photos/',null=True)
    fullname = models.CharField(max_length=200,null=True)
    username = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
--- gram/views.py
+++ gram/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Post,Profile,Comment,Like
 from django.http import HttpResponse
 # Create your views here.
 def welcome(request):
+    posts = Post.objects.all().order_by("-id")
+    profiles = Profile.objects.all()
+    current_user = request.user
+    
+    comments=Comment.objects.all()
+    likes = Like.objects.all()
+    
     return HttpResponse('Welcome to MoreGram')
