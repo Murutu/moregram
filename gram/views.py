9794403acdbf7cbf5ffe46c050035fecb7bb59c9
Murutu <murutupeter@email.com>
2020-03-08 20:37:47 +0300

    added import redirect and LikeForm in views.py

--- gram/views.py
+++ gram/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Post,Profile,Comment,Like
 from django.http import HttpResponse
 from .forms import NewsLetterForm,LikeForm
 # Create your views here.
+
+
 def welcome(request):
     posts = Post.objects.all().order_by("-id")
     profiles = Profile.objects.all()
@@ -22,5 +24,16 @@ def welcome(request):
     if request.method == 'POST' and 'liker' in request.POST:
         post_id = request.POST.get("liker")
         likeform = LikeForm(request.POST)
+        if likeform.is_valid():
+            post_id = int(request.Post.get("liker"))
+            like = likeform.save(commit=False)
+            like.username = request.user
+            like.post = post
+            like.control = str(like.username.id)+"-"+str(like.post.id)
+            like.save()
+            print("like saved")
+            return redirect("welcome")
+        else:
+            likeform = LikeForm()
     
     return HttpResponse('Welcome to MoreGram')
