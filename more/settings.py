c5739b5865e875a04ae1afcb3353d58cba4c17ec
Murutu <murutupeter@email.com>
2020-03-08 14:56:40 +0300

    added user/password at databases in settings.py

--- more/settings.py
+++ more/settings.py
@@ -78,7 +78,9 @@ WSGI_APPLICATION = 'more.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'gram',
+        'USER': 'peter',
+    'PASSWORD':'ozil',
     }
 }
 
