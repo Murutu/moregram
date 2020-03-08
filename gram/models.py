e92f9f43c1537c8fb8130d989b21deb286f3a539
Murutu <murutupeter@email.com>
2020-03-08 17:23:37 +0300

    added load static in base.html

--- gram/models.py
+++ gram/models.py
@@ -4,5 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(model.Model):
    profile_pic = models.ImageField(upload_to='photos/',null=True)
-   fullname = models.CharField(max_length=255,null=True)
+   fullname = models.CharField(max_length=200,null=True)
    username = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+   bio = models.TextField(max_length=100)
+   email = models.EmailField(null=True)
+   phonenumber = models.IntegerField(null=True)
