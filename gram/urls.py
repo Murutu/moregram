045853b519beed9c3c926bedec2858513a8c1250
Murutu <murutupeter@email.com>
2020-03-08 18:03:39 +0300

    added search,newpost and accounts in urls.py

--- gram/urls.py
+++ gram/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.welcome,name = 'Welcome to Instagram '),
+    path('',views.welcome,name = 'Welcome to MoreGram '),
     path('search/', views.search_results, name='search_results'),
+    path('accounts/profile/(\d+)',views.profile, name = 'profile'),
+    path('new/post',views.new_post, name ='new-post'),
+    path('accounts/edit-profile/',views.edit_profile,name = 'edit-profile'),
     
 ]
 
